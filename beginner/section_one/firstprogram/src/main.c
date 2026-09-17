9a55b881921380f8c6fb57e74c2e7f992be8650b
za <ajcpersonal@proton.me>
2026-09-17 20:21:11 +1000

    lit

---
+++ beginner/section_one/firstprogram/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int myFavouriteNumber = 0;
+
+    printf("What is your favourite number?\n");
+    scanf("%d", &myFavouriteNumber);
+    printf("You entered number %d\n", myFavouriteNumber);
+}
